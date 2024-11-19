df4a6551705584bf8570d62d7640c6b3547b62fd
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-11-20 03:07:09 +0900

    로그아웃

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -9,7 +11,21 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Prfile")),
+      appBar: AppBar(
+        title: Text("Prfile"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              semanticLabel: 'logout',
+            ),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+          )
+        ],
+      ),
     );
   }
 }
