f45b9556314b3cfab41978134340b68aebe84615
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-11-30 18:59:14 +0900

    마커 찍은 위치로 이동

--- lib/maps/getlocation.dart
+++ lib/maps/getlocation.dart
@@ -3,47 +3,76 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import '../provider.dart';
 
-class MapScreen extends StatefulWidget {
-  const MapScreen({Key? key}) : super(key: key);
-
+class GetLocation extends StatefulWidget {
   @override
-  _MapScreenState createState() => _MapScreenState();
+  _GetLocationState createState() => _GetLocationState();
 }
 
-class GetLocation extends StatelessWidget {
+class _GetLocationState extends State<GetLocation> {
+  late GoogleMapController _mapController;
+
+  Marker? _currentMarker; // 현재 위치를 표시할 마커
+
+  @override
+  void initState() {
+    super.initState();
+    final geoProvider = Provider.of<GeoProvider>(context, listen: false);
+    geoProvider.fetchGeoData(); // 앱 시작 시 위치 정보 가져오기
+  }
+
+  void _updateMarker(double latitude, double longitude) {
+    setState(() {
+      _currentMarker = Marker(
+        markerId: MarkerId("current_location"),
+        position: LatLng(latitude, longitude),
+        infoWindow: InfoWindow(title: "선택된 위치"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final geoProvider = Provider.of<GeoProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('모일 위치를 선택해주세요.'),
+        title: Text("현재 위치"),
       ),
-      body: const MapScreen(),
-    );
-  }
-}
-
-class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController mapController;
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final geoProvider = Provider.of<GeoProvider>(context, listen: false);
-
-    final double latitude = geoProvider.latitude ?? 37.7749; // 기본값 (샌프란시스코)
-    final double longitude = geoProvider.longitude ?? -122.4194; // 기본값 (샌프란시스코)
-
-    final LatLng _center = LatLng(latitude, longitude);
-
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 18.0,
-      ),
+      body: geoProvider.isLoading
+          ? Center(child: CircularProgressIndicator()) // 로딩 상태 표시
+          : geoProvider.errorMessage != null
+              ? Center(child: Text(geoProvider.errorMessage!)) // 에러 메시지 표시
+              : Stack(
+                  children: [
+                    GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(
+                          geoProvider.latitude ?? 0.0,
+                          geoProvider.longitude ?? 0.0,
+                        ),
+                        zoom: 18,
+                      ),
+                      mapType: MapType.normal,
+                      onMapCreated: (controller) {
+                        _mapController = controller;
+                        _updateMarker(
+                          geoProvider.latitude!,
+                          geoProvider.longitude!,
+                        );
+                      },
+                      markers: _currentMarker != null ? {_currentMarker!} : {},
+                      onTap: (LatLng coordinate) {
+                        _mapController.animateCamera(
+                          CameraUpdate.newLatLng(coordinate),
+                        );
+                        _updateMarker(
+                            coordinate.latitude, coordinate.longitude);
+                        print(coordinate); // 마커 찍은 위치
+                      },
+                    ),
+                  ],
+                ),
     );
   }
 }
