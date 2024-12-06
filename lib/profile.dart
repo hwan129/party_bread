a74430ac7bab0b54a99d55b5089d7c88da35e582
max4779 <max4779@naver.com>
2024-12-06 22:07:31 +0900

    profile fin
    profile fin

--- lib/profile.dart
+++ lib/profile.dart
@@ -58,8 +58,6 @@ class _ProfilePageState extends State<ProfilePage> {
             await _firestore.collection('bread').doc(docId).get();
 
         if (doc.exists) {
-          print('Fetched document: ${doc.data()}');  // 데이터 출력
-
           setState(() {
             activityHistory.add({
               'category': doc['category'] ?? '',
@@ -90,7 +88,229 @@ class _ProfilePageState extends State<ProfilePage> {
       });
     }
   }
+  Widget _listTileBuild(Map<String, dynamic> bread) {
+  print('Bread data: $bread');  // Bread 데이터 구조를 디버깅하기 위한 출력
 
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+    child: InkWell(
+      onTap: () => showBreadDetails(bread),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white, // 배경색 설정
+          borderRadius: BorderRadius.circular(10), // 둥근 모서리 설정
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2), // 그림자 색
+              spreadRadius: 2, // 그림자의 확장 범위
+              blurRadius: 5, // 그림자의 흐림 정도
+              offset: Offset(1, 1), // 그림자의 위치 (x, y 방향)
+            ),
+          ],
+        ),
+        child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                bread['category'] == '택시팟빵'
+                  ? "${bread['data']['탑승 장소']} -> ${bread['data']['목적지']}"
+                  : bread['category'] == '배달팟빵'
+                      ? "${bread['data']['음식 이름']}"
+                      : bread['category'] == '기타팟빵'
+                          ? "${bread['data']['이름']}"
+                          : bread['category'] == '공구팟빵'
+                            ? "${bread['data']['제품명']}"
+                            : "알 수 없는 카테고리",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF574142),
+                ),
+              ),
+            ],
+          ),
+          subtitle: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "${bread['category']}  |  ${bread['data']['현재 인원 수']} / ${bread['data']['인원 수']}  |  ${bread['마감일']}",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF574142),
+                ),
+              ),
+            ],
+          ),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () => {showBreadDetails(bread)},
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+void showBreadDetails(Map<String, dynamic> bread) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          heightFactor: 0.5,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(),
+                    IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+                if (bread['category'] == '택시팟빵') ...[
+                  Text(
+                    "출발지: ${bread['data']['탑승 장소'] ?? '정보 없음'}",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "목적지: ${bread['data']['목적지'] ?? '정보 없음'}",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  Text("탑승 시간: ${bread['data']['탑승 시간'] ?? '정보 없음'}"),
+                ] else if (bread['category'] == '배달팟빵')...[
+                  Text(
+                    bread['data']['음식 이름'] ?? '제목 없음',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("주문 시간: ${bread['data']['주문 시간'] ?? '알 수 없음'}"),
+                  Text("픽업 시간: ${bread['data']['픽업 시간'] ?? '미정'}"),
+                ]
+                else if (bread['category'] == '공구팟빵')...[
+                  Text(
+                    bread['data']['제품명'] ?? '제목 없음',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("마감일: ${bread['data']['마감일'] ?? '알 수 없음'}"),
+                ]
+                else if (bread['category'] == '기타팟빵')...[
+                  Text(
+                    bread['data']['이름'] ?? '제목 없음',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("마감일: ${bread['data']['마감일'] ?? '알 수 없음'}"),
+                  Text("장소: ${bread['data']['장소'] ?? '미정'}"),
+                ],
+                
+                SizedBox(height: 10),
+                Text(
+                  "현재 인원 수/인원 수: ${bread['data']['현재 인원 수'] ?? 0}/${bread['data']['인원 수'] ?? 0}",
+                ),
+                SizedBox(height: 10),
+                Text("세부사항: ${bread['data']['detail'] ?? '없음'}"),
+                Spacer(),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final user = FirebaseAuth.instance.currentUser;
+
+                        if (user != null) {
+                          // Firestore에서 user 컬렉션에 접근
+                          final userDoc = FirebaseFirestore.instance
+                              .collection('user')
+                              .doc(user.uid);
+
+                          // 유저의 interactedDocs 배열에 해당 문서 ID가 존재하는지 확인
+                          final userSnapshot = await userDoc.get();
+                          final interactedDocs = List<String>.from(
+                              userSnapshot.data()?['interactedDocs'] ?? []);
+
+
+
+                          // 문서 ID가 없으면 추가
+                          if (!interactedDocs.contains(bread['docId'])) {
+                            // 'currentpeopleCount'와 'peopleCount'가 null이 아니고, int로 변환 가능한지 체크
+                            if (bread['currentpeopleCount'] != null && bread['peopleCount'] != null) {
+                              try {
+                                // String을 int로 변환
+                                int currentPeopleCount = int.tryParse(bread['currentpeopleCount'].toString()) ?? 0;
+                                int peopleCount = int.tryParse(bread['peopleCount'].toString()) ?? 0;
+
+                                // 인원 수가 같지 않으면
+                                if (currentPeopleCount != peopleCount) {
+                                  await userDoc.update({
+                                    'interactedDocs': FieldValue.arrayUnion([bread['docId']]) // 문서 ID 추가
+                                  });
+
+                                  // Firestore에서 해당 bread 문서의 '현재 인원 수' 증가
+                                  final breadDoc = FirebaseFirestore.instance
+                                      .collection('bread')
+                                      .doc(bread['docId']);
+                                  final breadSnapshot = await breadDoc.get();
+                                  String currentPeopleCountStr = breadSnapshot.data()?['data']['현재 인원 수']?.toString() ?? '0';
+
+                          // int로 변환
+                                  int currentPeopleCount = int.tryParse(currentPeopleCountStr) ?? 0;
+                                  await breadDoc.update({
+                                    'data.현재 인원 수': currentPeopleCount + 1, // 현재 인원 수 +1
+                                  });
+
+                                  // 채팅 화면으로 이동
+                                  Navigator.pushNamed(
+                                    context,
+                                    '/chatting',
+                                    arguments: {'roomId': bread['category']},
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text('인원이 다 찼습니다!')),
+                                  );
+                                }
+                              } catch (e) {
+                                print('변환 오류 발생: $e');
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('숫자 변환 중 오류가 발생했습니다.')),
+                                );
+                              }
+                            }
+                          } else {
+                            Navigator.pushNamed(
+                              context,
+                              '/chatting',
+                              arguments: {'roomId': bread['category']},
+                            );
+                          }
+                        }
+                      } catch (e) {
+                        print('문서 업데이트 중 오류 발생: $e');
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('오류가 발생했습니다. 다시 시도해주세요.')),
+                        );
+                      }
+                    },
+                    child: Text('팟빵 함께 먹기'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -191,52 +411,17 @@ class _ProfilePageState extends State<ProfilePage> {
               itemCount: activityHistory.length,
               itemBuilder: (context, index) {
                 final activity = activityHistory[index];
-                final data = activity['data'] ?? {}; // data가 null일 경우 빈 Map을 사용
+                //final data = activity['data'] ?? {}; // data가 null일 경우 빈 Map을 사용
                 final category = activity['category'] ?? 'No Category'; // 카테고리 확인
 
-                // Debugging: data 출력 확인
-                print('Category: $category');
-                print('Data: $data');
-
-                // 카테고리별로 title과 subtitle 설정
-                String title = '';
-                String subtitle = '';
-
-                if (category == "택시팟빵") {
-                  // 택시팟빵에 맞는 title과 subtitle 설정
-                  title =
-                      "${data['탑승 장소'] ?? '출발지 없음'} → ${data['목적지'] ?? '목적지 없음'}";
-                  subtitle = data['추가 사항'] ?? '추가 사항 없음';
-                } 
-                
-                else if (category == "배달팟빵") {
-                  title = data['음식 이름'] ?? '이름 없음';
-                  subtitle = data['추가 사항'] ?? '추가 사항 없음';
-                } 
-                
-                else if (category == "공구팟빵") {
-                  title = data['제품명'] ?? '이름 없음';
-                  subtitle = data['추가 사항'] ?? '추가 사항 없음';
-                } 
-                
-                else if (category == "기타팟빵") {
-                  title = data['이름'] ?? '이름 없음';
-                  subtitle = data['추가 사항'] ?? '추가 사항 없음';
-                } 
-        
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  elevation: 4.0,
-                  child: ListTile(
-                    title: Text(title),
-                    subtitle: Text(subtitle),
-                  ),
-                );
+                // activity 데이터를 _listTileBuild에 전달
+                return _listTileBuild(activity);
               },
             ),
     ],
   ),
 )
+
 
 
 
